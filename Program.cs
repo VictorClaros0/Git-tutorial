67f06e282a97a46957ade18deb8c767e4e2cc3b6
VictorClaros0 <cgv0033076@est.univalle.edu>
2023-05-10 14:16:34 -0400

    Version 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nro1, nro2;
+            Random rd = new Random();
+            nro1= rd.Next(0,10000);
+            nro2 = rd.Next(0, 10000);
+            Console.WriteLine(nro1+ " " + nro2);
+            Console.WriteLine(nro1+nro2);
+        }
+    }
+}
