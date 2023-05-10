b20af2a5d8764c19315c561c2eb23e7978f236b9
VictorClaros0 <cgv0033076@est.univalle.edu>
2023-05-10 14:24:31 -0400

    Version 2

--- Program.cs
+++ Program.cs
@@ -4,12 +4,14 @@
     {
         static void Main(string[] args)
         {
+            string x;
             int nro1, nro2;
             Random rd = new Random();
             nro1= rd.Next(0,10000);
             nro2 = rd.Next(0, 10000);
             Console.WriteLine(nro1+ " " + nro2);
             Console.WriteLine(nro1+nro2);
+            x= Console.ReadLine();
         }
     }
 }
